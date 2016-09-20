e59d962f75b9eae91db6c9162b7db6e6a5d34ec7
alecfullofmer <arfullmer@verisk.com>
2016-09-20 16:24:50 -0600

    added dashboard and some routing

---
+++ app/heroes.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import { Hero } from './hero';
+import { HeroService } from './hero.service';
+import { OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+
+@Component({
+  selector: 'my-heroes',
+  templateUrl: 'app/heroes.component.html',
+  styles: ['app/heroes.component.css'],
+  providers: [HeroService]
+})
+
+export class HeroesComponent implements OnInit {
+  
+  constructor(private router: Router, private heroService: HeroService) { }
+    
+    selectedHero: Hero;
+    heroes: Hero[];
+    
+    ngOnInit(): void {
+      this.getHeroes();
+    }
+ 
+     getHeroes(): void {
+      this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+    }
+    onSelect(hero: Hero): void {
+      this.selectedHero = hero;
+    }
+    
+    gotoDetail(): void {
+        this.router.navigate(['/detail', this.selectedHero.id]);
+    }
+}
+
